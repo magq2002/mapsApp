12f1e9752f5aac36b6c53ef1edee9f42ea69d00f
AsusMiguel <migalvez2002@gmail.com>
2024-04-16 10:00:01 -0500

    view markers and properties added

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
+
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
 export class MiniMapComponent {
 
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+
+  public map?: Map;
+
+
+  ngAfterViewInit(){
+    if ( !this.divMap?.nativeElement ) throw "Map Div not found";
+    if ( !this.lngLat ) throw "LngLat can't be null";
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, 
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false,
+  });
+
+  new Marker()
+    .setLngLat( this.lngLat )
+    .addTo( this.map )
+  }
 }
